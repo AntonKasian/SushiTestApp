2842a5e09e59455f0a0334e8fb601f7ed2022698
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-09-21 11:22:33 +0400

    Create custom navigation view

--- SushiTestApp/ViewController/HomeViewController.swift
+++ SushiTestApp/ViewController/HomeViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 class HomeViewController: UIViewController {
     
     let backColor = #colorLiteral(red: 0.1411764324, green: 0.1411764324, blue: 0.1411764324, alpha: 1)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = backColor
-        navigationItem.title = "Title"
+        setupViews()
+        setupNavigationBar()
+        setupRightBarButton()
     }
+    
+    private func setupViews() {
+        view.backgroundColor = backColor
+    }
+    
+    //MARK: - NavigationView
+    
+    private func setupNavigationBar() {
+        let customView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 44))
+        
+        let imageIcon = UIImageView(image: UIImage(systemName: "circle.circle")?.withTintColor(.label, renderingMode: .alwaysOriginal))
+        imageIcon.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        let customLabel = UILabel(frame: CGRect(x: 44, y: 0, width: 150, height: 44))
+        customLabel.text = "VKUSSOVET"
+        customLabel.textColor = .label
+        customLabel.font = UIFont.boldSystemFont(ofSize: 22)
+        customView.addSubview(imageIcon)
+        customView.addSubview(customLabel)
+        
+        let customBarButtonItem = UIBarButtonItem(customView: customView)
+        navigationItem.leftBarButtonItem = customBarButtonItem
+    }
+
+    
+    private func setupRightBarButton() {
+        let phoneImage = UIImage(systemName: "phone")?.withTintColor(.label, renderingMode: .alwaysOriginal)
+        
+        let phoneButton = UIBarButtonItem(image: phoneImage, style: .plain, target: self, action: #selector(phoneButtonTapped))
+        
+        if let font = UIFont(name: "System", size: 22) {
+            phoneButton.setTitleTextAttributes([NSAttributedString.Key.font: font], for: .normal)
+        }
+        
+        navigationItem.rightBarButtonItem = phoneButton
+    }
+    
+    
+    @objc func phoneButtonTapped() {
+        print("Phone button tapped")
+    }
+    
 }
 
