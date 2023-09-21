8b0780f64f6c2e5043535ef09304a5e19a548c4d
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-09-21 10:15:08 +0400

    Initial commit

---
+++ SushiTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SushiTestApp
+//
+//  Created by Anton on 21.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
